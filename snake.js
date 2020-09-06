8d4931e28564d6cfd22740ba62badd6795c5614e
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-05 19:48:30 -0500

    ya se mueve en la direccion que le voy indicando

--- snake.js
+++ snake.js
@@ -1,22 +1,28 @@
-
 // creamos el constructor
 function Snake() {
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = 1;
-    this.yspeed = 0;
-  
-    // para estar actualizando los valores
-    this.update = function () {
-      this.x = this.x + this.xspeed;
-      this.y = this.y + this.yspeed;
-    };
-  
-    // para mostrar y dibujar un rectangulo
-    this.show = function () {
-      //   pintamos el rectangulo blanco
-      fill(255);
-      //   hacemos el rectangulo de 10x10
-      rect(this.x, this.y, 10, 10);
-    };
-  }
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+
+  //   le pasamos valores x , y para darle direccion al objeto
+  // en este caso el objeto es la serpiente
+  this.dir = function (x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  };
+
+  // para estar actualizando los valores
+  this.update = function () {
+    this.x = this.x + this.xspeed;
+    this.y = this.y + this.yspeed;
+  };
+
+  // para mostrar y dibujar un rectangulo
+  this.show = function () {
+    //   pintamos el rectangulo blanco
+    fill(255);
+    //   hacemos el rectangulo de 10x10
+    rect(this.x, this.y, 10, 10);
+  };
+}
