cf3c6fee4a7158ab4bd055f03733b1a8d0f57acc
Eduardo Sandoval <edubizzle7@gmail.com>
2020-09-05 19:45:49 -0500

    se creo otro archivo de js para organizar el codigo

---
+++ snake.js
@@ -0,0 +1,22 @@
+
+// creamos el constructor
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+  
+    // para estar actualizando los valores
+    this.update = function () {
+      this.x = this.x + this.xspeed;
+      this.y = this.y + this.yspeed;
+    };
+  
+    // para mostrar y dibujar un rectangulo
+    this.show = function () {
+      //   pintamos el rectangulo blanco
+      fill(255);
+      //   hacemos el rectangulo de 10x10
+      rect(this.x, this.y, 10, 10);
+    };
+  }
